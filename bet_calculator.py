766da17466426b216d2b6dea30ab6e269a33b0cf
Zyphlen Kotani <zkotani@gmail.com>
2022-10-07 10:00:53 -0400

    Added main python file -- contains header

---
+++ bet_calculator.py
@@ -0,0 +1,8 @@
+'''
+Name -- bet_calculator
+Version -- 1.1
+Description -- Simple python script used to automate sports betting.
+    Calculates amount for individual bets based on percentages of a main pool.
+Developer -- Zyphlen Kotani [zkotani@gmail.com]
+Github -- http://github.com/zkotani
+'''
